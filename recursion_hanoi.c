8ff506aed2e05f64f4f76261b3530f215303d798
Ananya <acharyananya06@gmail.com>
2025-07-23 19:49:47 +0530

    Create recursion_hanoi.c

---
+++ recursion_hanoi.c
@@ -0,0 +1,19 @@
+//WAP in C to code the hanoi tower
+
+#include <stdio.h>
+
+void TOH(int n,int A,int B,int C)
+{
+	if(n>0)
+	{
+		TOH(n-1,A,C,B);
+		printf("(%d,%d)\n",A,C);
+		TOH(n-1,B,A,C);
+	}
+}
+
+int main()
+{
+TOH(3,1,2,3);
+return 0;
+}
